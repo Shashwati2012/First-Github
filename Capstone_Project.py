74c4d124fc9ff6a02205ea6a795f6f0557173dff
Shashwati Dutta <shashwati.dutta2012@gmail.com>
2019-12-24 22:32:13 +0530

    Adding Capstone Project Notebook

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# # This Notebook will mainly be used for Capstone Project
 
+# In[6]:
 
-##This Notebook will be used mainly for Capstone Proeject
 
 import numpy as np
 import pandas as pd
 
 
-# In[6]:
-
-
-print("Hello Capstone Project Course!")
-
-
 # In[ ]:
 
 
-
+print("Hello Capstone Project Course!")
 
